2f71919615fe7c9484f4dc5366133f49f849fc93
Katios <kate.chonkina@gmail.com>
2021-02-02 12:47:06 +0600

    added git ignore

---
+++ .gitignore.swift
@@ -0,0 +1,15 @@
+
+
+import SwiftUI
+
+struct _gitignore: View {
+    var body: some View {
+        Text("Error")
+    }
+}
+
+struct _gitignore_Previews: PreviewProvider {
+    static var previews: some View {
+        _gitignore()
+    }
+}
---
+++ logs.swift
@@ -0,0 +1,20 @@
+//
+//  logs.swift
+//  BookCoreData
+//
+//  Created by Екатерина Чернова on 02.02.2021.
+//
+
+import SwiftUI
+
+struct logs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct logs_Previews: PreviewProvider {
+    static var previews: some View {
+        logs()
+    }
+}
