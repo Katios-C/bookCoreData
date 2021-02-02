b9c3cc506fae434f94507cb060bd6ca278835d88
Katios <kate.chonkina@gmail.com>
2021-02-02 14:34:17 +0600

    create filereadmi

---
+++ readme.swift
@@ -0,0 +1,20 @@
+//
+//  readme.swift
+//  BookCoreData
+//
+//  Created by Екатерина Чернова on 02.02.2021.
+//
+
+import SwiftUI
+
+struct readme: View {
+    var body: some View {
+        Text("Readme file")
+    }
+}
+
+struct readme_Previews: PreviewProvider {
+    static var previews: some View {
+        readme()
+    }
+}
