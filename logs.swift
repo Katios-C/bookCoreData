d541b4400c525bdb224b9ef59336e18f08b9febd
Katios <kate.chonkina@gmail.com>
2021-02-02 16:16:34 +0600

    added some strings

--- logs.swift
+++ logs.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct logs: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Once more commit")
     }
 }
 
